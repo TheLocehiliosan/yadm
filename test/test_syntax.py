84437894b51e4c28dac6e0e05cb2e4dbaba9b479
Tim Byrne <sultan@locehilios.com>
2023-07-08 14:46:46 -0500

    Use man.REAL

--- test/test_syntax.py
+++ test/test_syntax.py
@@ -59,7 +59,7 @@ def test_yamllint(pytestconfig, runner, yamllint_version):
 def test_man(runner):
     """Check for warnings from man"""
     run = runner(
-        command=['man', '--warnings', './yadm.1'])
+        command=['man.REAL', '--warnings', './yadm.1'])
     assert run.success
     assert run.err == ''
     assert 'yadm - Yet Another Dotfiles Manager' in run.out
